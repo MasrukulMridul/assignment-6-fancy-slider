12608c140f43f75a328fc219f99348b44aa2a3d8
MasrukulMridul <masrukulmridul15@gmail.com>
2021-02-14 16:29:39 +0600

    final code

--- app.js
+++ app.js
@@ -14,13 +14,24 @@ let sliders = [];
 const KEY = '15674931-a9d714b6e9d654524df198e00&q';
 
 //keyboard enter key
-
 document.getElementById("search").addEventListener("keypress", function(event) {
     if (event.key === 'Enter'){
       document.getElementById("search-btn").click();
     }
    
 });
+// Bonus part spinner add
+
+const toggleSpinner =(show) => {
+  const spinner = document.getElementById('loading-spinner');
+  if(show){
+    spinner.classList.remove('d-none');
+  }
+  else{
+    spinner.classList.add('d-none');
+  }
+  
+}
 
 // show images 
 const showImages = (images) => {
@@ -32,28 +43,37 @@ const showImages = (images) => {
     let div = document.createElement('div');
     div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
     div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
-    gallery.appendChild(div)
+    gallery.appendChild(div);
+    toggleSpinner(false);
   })
 
 }
 
 const getImages = (query) => {
+  toggleSpinner(true);
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
-    .then(data => showImages(data.hits))
-    .catch(err => console.log(err))
+    .then(data => {if(data.hits.length){
+      showImages(data.hits);
+    }
+    else{
+      alert('Sorry!!This type of Picture Not Available ')    //Another Bonus Part
+
+    }})
+    .catch(err=> console.log(err))
+   
 }
 
 let slideIndex = 0;
 const selectItem = (event, img) => {
   let element = event.target;
-  element.classList.add('added');
- 
   let item = sliders.indexOf(img);
+  
   if (item === -1) {
+    element.classList.toggle('added');
     sliders.push(img);
   } else {
-    alert('Hey, Already added !')
+    element.classList.toggle('added');
   }
 }
 var timer
@@ -129,3 +149,4 @@ searchBtn.addEventListener('click', function () {
 sliderBtn.addEventListener('click', function () {
   createSlider()
 })
+
